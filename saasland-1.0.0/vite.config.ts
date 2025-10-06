719616a20d56e2837c165ff4ed78c39b7896b963
ASMAA <elhintasmaa@gmail.com>
2025-10-07 00:29:50 +0100

    Fix base path for Vercel

--- saasland-1.0.0/vite.config.ts
+++ saasland-1.0.0/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: "/saasland/",
+export default defineConfig({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -29,4 +29,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-}));
+});
